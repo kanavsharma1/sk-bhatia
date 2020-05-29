1d0f5fbe084d65d983f14c2967d679a27874b857
kanav sharma <kanavsharma1@outlook.com>
2020-05-29 21:59:41 +0530

    committing changes on navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,136 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-
-const drawerWidth = 240;
+import { Button, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+    flexGrow: 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  hide: {
-    display: "none",
+  title: {
+    flexGrow: 1,
+    textDecoration: "none",
+    color: "#f5f5f5",
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
+  navLink: {
+    textDecoration: "none",
+    color: "#f5f5f5",
+    padding: 20,
   },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
+  buttonText: {
+    color: "#f5f5f5",
+    fontWeight: 600,
   },
 }));
 
 export const NavBar = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  const [menuItems] = React.useState([
-    "Home",
-    "About us",
-    "Services",
-    "Sectors",
-  ]);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const [menuItems] = React.useState(["Home", "About", "Services", "Sectors"]);
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            sk bhatia
+    <AppBar position="sticky">
+      <Toolbar>
+        <Link className={classes.title} to="/">
+          <Typography variant="h4" className={classes.title}>
+            SK BHATIA
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "ltr" ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {menuItems.map((text, index) => (
-            <Link to={text} key={index}>
-              <ListItem button key={index}>
-                <ListItemText primary={text} />
-              </ListItem>
-            </Link>
-          ))}
-        </List>
-      </Drawer>
-    </div>
+        </Link>
+        {menuItems.map((item, index) => (
+          <Link key={index} className={classes.navLink} to={item}>
+            <Button size="large" key={index}>
+              <Typography variant="h6" className={classes.buttonText}>
+                {item}
+              </Typography>
+            </Button>
+          </Link>
+        ))}
+      </Toolbar>
+    </AppBar>
   );
 };
--- src/components/services-component/services.js
+++ src/components/services-component/services.js
@@ -4,25 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { JumbotronBar } from "../jumbotron/jumbotron";
 import BannerServiceImage from "../../images/services-jumbotron.jpg";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
 export const Services = () => {
-  const classes = useStyles();
   return (
     <Grid container>
       <JumbotronBar banner={BannerServiceImage} />
